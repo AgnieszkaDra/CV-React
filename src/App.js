ab980284a7490d863f1b4205045b22be264e5344
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-07-19 17:45:35 +0200

    add some changes to header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import logo from '../src/assets/face.jpg';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faPhone, faGlobe, faLink} from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faPhone, faGlobe, faLink } from '@fortawesome/free-solid-svg-icons'
 
 
 function App() {
@@ -21,20 +21,41 @@ function App() {
                 <a href={'//mailto: agnieszka.127@interia.pl'} target="_blank">agnieszka.127@interia.pl</a>
               </li>
               <li>
-              <FontAwesomeIcon icon={faPhone} />
+                <FontAwesomeIcon icon={faPhone} />
                 <a href={'//tel:+48517862170'} target="_blank"> +48 517862170 </a>
               </li>
               <li>
-              <FontAwesomeIcon icon={faGlobe} />
+                <FontAwesomeIcon icon={faGlobe} />
                 <a href={'//tel:+48517862170'} target="_blank"> Portfolio </a>
               </li>
-                <li>
-              <FontAwesomeIcon icon={faLink} />
+              <li>
+                <FontAwesomeIcon icon={faLink} />
                 <a href={'//tel:+48517862170'} target="_blank"> Linkedln </a>
               </li>
               <li>
-              <FontAwesomeIcon icon={faLink} />
+                <FontAwesomeIcon icon={faLink} />
                 <a href={'//tel:+48517862170'} target="_blank"> Github </a>
+              </li>
+            </ul>
+          </div>
+          <div className="education-container container-block">
+            <h2 className="container-block-title">Job</h2>
+            <div className="item">
+              <h4 className="degree">Master of Pharmacy</h4>
+              <h5 className="meta">Work at pharmacist</h5>
+              <div className="time">2013-Present</div>
+            </div>
+          </div>
+          <div className="languages-container container-block">
+            <h2 className="container-block-title">Languages</h2>
+            <ul className="list-unstyled interests-list" key="lang_list">
+              <li key='language-item'>
+                POLISH
+                <span className="lang-desc">Native</span>
+              </li>
+              <li key='language-item'>
+                ENGLISH
+                <span className="lang-desc">B1/B2</span>
               </li>
             </ul>
           </div>
