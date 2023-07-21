8161225652b68354e64006bc08e05c3c7eaab778
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-07-22 00:23:13 +0200

    some trials in ContactsList.js

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -2,38 +2,89 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPhone, faGlobe, faLink } from '@fortawesome/free-solid-svg-icons'
 
-class ContactsList extends React.Component{
-  
+class ContactsList extends React.Component {
+
     // renderListItem(item, i) {
     //     return (
     //         <li key={`language_item_${i}`}>
     //         {item.key}
-            
+
     //       </li>
     //     );
     //   }
+    // return Object.entries(item).map(([key, values]) => {
+    //     return <li>{key}:{values}</li>
+    // })
 
-    renderListItem(item,i) {
-        return Object.entries(item).map(([key, values]) => {
-            return <li>{key}:{values}</li>
-        })
+    pokaz(el) {
+      return Object.entries(el).map(([key, values]) => {
+         return <>{key}{values}</>
+      })
     }
-    render(){
+    renderListItem(item, i) {
+        return <li>{item[0]}{this.pokaz(item[1])}</li>
+    //    return Object.entries(item).map(([key, values]) => {
+    //         return <li>{key}:{values}</li>
+    //     })
+    } 
+    
+    renderOdd(item,i) {
+        
+        return item%2
+    }
+
+    odd(item, list) {
+
+        function isObject(variable) {
+            if(typeof variable === 'object' && variable !== null){
+               return console.log(variable)
+                }}
+            
+          
+
+          function isString(variable) {
+            return typeof variable === 'string' && variable !== null;
+          }
+
+        function filterObjectsFromArray(arr) {
+            return arr.filter(item => isObject(item));
+          }
+
+          function filterStringsFromArray(arr) {
+            return arr.filter(item => isString(item));
+          }
+       
+        const objectsArray = filterObjectsFromArray(list);
+        const stringArray = filterStringsFromArray(list)
+
+        return <li>{stringArray}</li>
+
+       
+    }
+   
+
+   
+    render() {
        
         return (
-         
-            <ul className="" key="">
-            {this.props.list.map((item, i) => {
-            return this.renderListItem(item,i)
-          })}
-          </ul>
-         
-          
-        
-      
             
-      
-          
+            <ul className="" key="">
+              
+                  {/* {this.props.list.map((item,i) => {
+                    const list = this.props.list
+                    return this.odd(item,list)
+                  })} */}
+                {this.props.list.map((item, i) => {
+                    return this.renderListItem(item, i)
+                })}
+            </ul>
+
+
+
+
+
+
+
         )
     }
 }
--- src/data/infoProfile.js
+++ src/data/infoProfile.js
@@ -2,12 +2,12 @@ const infoProfile = {
     profileBasic :{ name: 'Agnieszka Dragańczyk', title: 'Junior Front End Developer'},
     profileList: {
       list: [
-        { mail: 'agnieszka.127@interia.pl'},
-        { phoneNumber: '+48 517 862 170'},
-        { portfolio: ''},
-        { linkedin: ''},
-        { github: ''},
-        { imagePath: ''}
+        ['fa-envelope', { mail: 'agnieszka.127@interia.pl'}],
+        ['fa-phone', { phoneNumber: '+48 517 862 170' }],
+        ['fa-globe',{ portfolio: ''}]
+        // { linkedin: ''},
+        // { github: ''},
+        // { imagePath: ''}
       ]
     } 
         
