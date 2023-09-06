d15db3fd57c1b1d0c8ed827f5d8257def4610195
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-09-06 17:36:34 +0200

    add build folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import './App.css'
 
 import Header from './components/HeaderContainer/Header'
 import Main from './components/Main/Main'
-import './styles/index.css'
+import './styles/index.scss'
 import data from './data'
 import PropTypes from 'prop-types'
 
 export default class CV extends React.Component {
   render () {
     return (
-      <>
+      <div className={'container'}>
         <Header
-            {...data.infoProfile}
-            className={'header'}
+          {...data.infoProfile}
+          className={'header'}
         >
         </Header>
         <Main {...data}></Main>
-      </>
+      </div>
     )
   };
 }
