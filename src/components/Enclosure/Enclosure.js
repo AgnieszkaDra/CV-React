e413b93b29ca60f43996a7a40fec3b97d2bbedef
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2024-01-21 17:53:34 +0100

    add change in enclosure

--- src/components/Enclosure/Enclosure.js
+++ src/components/Enclosure/Enclosure.js
@@ -15,12 +15,13 @@ export const Enclosure = () => {
   const enclosure = {
     polish: `Wyrażam zgodę na przetwarzanie moich danych osobowych przez ${companyName}  w celu prowadzenia rekrutacji na aplikowane przeze mnie stanowisko.
     Wyrażam zgodę na przetwarzanie moich danych osobowych w zakresie przyszłych procesów rekrutacyjnych.`,
+    polish2: 'Wyrażam zgodę na przetwarzanie danych osobowych zawartych w niniejszym dokumencie do realizacji procesu rekrutacji zgodnie z ustawą z dnia 10 maja 2018 roku o ochronie danych osobowych (Dz. Ustaw z 2018, poz. 1000) oraz zgodnie z Rozporządzeniem Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE (RODO).',
     english: `I hereby consent to my personal data being processed by ${companyName} for the purpose of considering my application for the vacancy advertised `
   }
 
   return (
     <>
-      {selectedLanguage === '' && <p>{enclosure.polish}</p>}
+      {selectedLanguage === '' && <div><p>{enclosure.polish}</p><p>{enclosure.polish2}</p></div>}
       {selectedLanguage === 'polish' && <p>{enclosure.polish}</p>}
       {selectedLanguage === 'english' && <p>{enclosure.english}</p>}
       <div className={`${confirmationClass} ${classes.container}`} >
