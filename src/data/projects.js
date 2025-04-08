5dee108c906936642af8e043ca37e412c5060b14
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2025-04-08 20:27:44 +0200

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,22 +2,22 @@ export const projects = {
   description: ',COMPLETED OR STILL HAVE BEEN DEVELOPING are listed below',
   examples: [
     {
-      title: 'Shop TS. This project is still in the developing process',
+      title: 'Shop TS. Still in the developing process',
       url: 'https://github.com/AgnieszkaDra/shop-TS',
       description: 'This project is an e-commerce web application built using TypeScript, json-server, and Vite. The application features a client-side interface for users to browse products, manage a shopping cart, and place orders. On the admin side, there’s an interface for managing orders and tracking their status.'
     },
+    // {
+    //   title: 'Pharmacy Website-React',
+    //   url: 'https://luminous-dusk-95a487.netlify.app/',
+    //   description: 'The Pharmacy Website Project is a web application with a specialized form that allows users to order drugs and other medications. The project leverages the power of React to create a dynamic and responsive user experience'
+    // },
+    // {
+    //   title: 'Basic Page React',
+    //   url: 'https://mellifluous-profiterole-46d3ae.netlify.app/',
+    //   description: 'Basic-Page-React is a single page website (SPA), which could serve a business card'
+    // },
     {
-      title: 'Pharmacy Website-React',
-      url: 'https://luminous-dusk-95a487.netlify.app/',
-      description: 'The Pharmacy Website Project is a web application with a specialized form that allows users to order drugs and other medications. The project leverages the power of React to create a dynamic and responsive user experience'
-    },
-    {
-      title: 'Basic Page React',
-      url: 'https://mellifluous-profiterole-46d3ae.netlify.app/',
-      description: 'Basic-Page-React is a single page website (SPA), which could serve a business card'
-    },
-    {
-      title: 'Hall Reservation',
+      title: 'Hall Reservation. Still in the developing process',
       url: 'https://github.com/AgnieszkaDra/Hall-Reservation',
       description: 'This project is a Hall Reservation System being developed in pure TypeScript, following Object-Oriented Programming (OOP) principles. The goal is to create a structured, maintainable, and scalable application for managing hall reservations efficiently'
     }
